6205f42149b701bbee6d541d748bdd38684f7f43
z-alzayer <zba21@ic.ac.uk>
2023-07-16 15:36:17 +0100

    added trains and basic mechanic

---
+++ Scripts/Carriage_Body.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+var force = Vector2(400, 0)
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = force.x
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
