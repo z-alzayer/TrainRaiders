8273587ff257abe30c9eb16135a0cac03763ade0
z-alzayer <zba21@ic.ac.uk>
2023-07-17 21:16:33 +0100

    added train tracks and double jump

--- Scripts/Carriage_Body.gd
+++ Scripts/Carriage_Body.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 var force = Vector2(100, 0)
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var _animated_sprite = $AnimatedSprite2D
+func _ready():
+	_animated_sprite.play("default")
+
+
+
 func _process(delta):
 	velocity.x = force.x
 	if not is_on_floor():
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 700.0
-const JUMP_VELOCITY = -1000.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -700.0
+var double_jump
+var jump_counter
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	double_jump = true
+	jump_counter = 1
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,9 +19,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and double_jump:
 		velocity.y = JUMP_VELOCITY
-
+		jump_counter -= 1
+	
+	if jump_counter == 0:
+		double_jump = false
+	
+	if is_on_floor():
+		jump_counter = 1
+		double_jump = true
+	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
